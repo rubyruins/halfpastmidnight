ffd64a35e9a97032185447f7c7cd67cf3c8cbafe
rubyruins <soumya.parekh@somaiya.edu>
2022-07-18 02:09:42 +0530

    style: Add order changes with icon in sorting

--- src/components/middle/sortbutton.js
+++ src/components/middle/sortbutton.js
@@ -1,19 +1,24 @@
 import Isotope from "isotope-layout"
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+
+config.autoAddCss = false
 
 const SortButton = () => {
-	const isotope = React.useRef()
-	const [sortKey, setSortKey] = React.useState('title')
-	// console.log("Sort key: " + sortKey);
+	const isotope = React.useRef();
+	const [sortKey, setSortKey] = React.useState('title');
+	const [prevSortKey, setPrevSortKey] = React.useState('title');
+	const [sortOrder, setSortOrder] = React.useState(true);
   
 	React.useEffect(() => {
 	  	isotope.current = new Isotope('.sort-container', {
 			itemSelector: '.postlisting',
 			masonry: {
-				// columnWidth: 100,
 				horizontalOrder: true
 			},
-			// layoutMode: 'vertical',
 			getSortData: {
 				rating: '.rating',
 				title: '.title',
@@ -25,27 +30,50 @@ const SortButton = () => {
   
 	React.useEffect(() => {
 
-		console.log(sortKey);
-		isotope.current.arrange({sortBy: sortKey, sortAscending: false})
-
+		isotope.current.arrange({sortBy: sortKey, sortAscending: sortOrder})
 		if (document !== undefined) {
 			var allButtons = document.querySelectorAll(".sort-button");
 			[].forEach.call(allButtons, function(button) {
 				button.classList.remove("sort-button-clicked");
+				var allIcons = button.querySelectorAll(".sort-button-icon");
+				[].forEach.call(allIcons, function(icon) {
+					icon.classList.add("sort-button-icon-hide");
+				});
 			});
-
 			var clickedButton = document.querySelector(".sort-" + sortKey);
 			clickedButton.classList.add("sort-button-clicked");
+			clickedButton.querySelector('.sort-order-' + sortOrder.toString()).classList.remove('sort-button-icon-hide');
 		}
-	}, [sortKey])
+	}, [sortKey, sortOrder]);
   
-	const handleSortKeyChange = key => () => setSortKey(key)
+	function handleSortKeyChange(key) {
+		console.log(key, "clicked");
+		if (prevSortKey === key) {
+			setSortOrder(!sortOrder);
+		} else {
+			setSortOrder(true);
+			setSortKey(key);
+		}
+		setPrevSortKey(key);
+	}
   
 	return (
 		<ul className="sort-button-group p-0">
-			<button onClick={handleSortKeyChange('title')} title="Sort by title" aria-label="Sort by title" className="sort-button sort-title">Title</button>
-			<button onClick={handleSortKeyChange('date')} title="Sort by date" aria-label="Sort by date" className="sort-button sort-date">Date</button>
-			<button onClick={handleSortKeyChange('rating')} title="Sort by rating" aria-label="Sort by rating" className="sort-button sort-rating">Rating</button>
+			<button onClick={() => handleSortKeyChange('title')} title="Sort by title" aria-label="Sort by title" className="sort-button sort-title">
+				Title
+				<FontAwesomeIcon icon={faArrowUp} size="1x" className="ps-2 sort-order-true sort-button-icon"/>
+				<FontAwesomeIcon icon={faArrowDown} size="1x" className="ps-2 sort-order-false sort-button-icon sort-button-icon-hide"/>
+			</button>
+			<button onClick={() => handleSortKeyChange('date')} title="Sort by date" aria-label="Sort by date" className="sort-button sort-date">
+				Date
+				<FontAwesomeIcon icon={faArrowUp} size="1x" className="ps-2 sort-order-true sort-button-icon sort-button-icon-hide"/>
+				<FontAwesomeIcon icon={faArrowDown} size="1x" className="ps-2 sort-order-false sort-button-icon sort-button-icon-hide"/>
+			</button>
+			<button onClick={() => handleSortKeyChange('rating')} title="Sort by rating" aria-label="Sort by rating" className="sort-button sort-rating">
+				Rating
+				<FontAwesomeIcon icon={faArrowUp} size="1x" className="ps-2 sort-order-true sort-button-icon sort-button-icon-hide"/>
+				<FontAwesomeIcon icon={faArrowDown} size="1x" className="ps-2 sort-order-false sort-button-icon sort-button-icon-hide"/>
+			</button>
 		</ul>
 	)
 }
