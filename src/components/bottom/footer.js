9bc08763104b26e1d548a384a0386307ae955903
rubyruins <soumya.parekh@somaiya.edu>
2022-07-19 02:17:40 +0530

    style: Refactor and generalize styles

--- src/components/bottom/footer.js
+++ src/components/bottom/footer.js
@@ -3,8 +3,8 @@ import * as React from 'react'
 const Footer = ({title}) => {
 
   return (
-	<footer className="py-5 bg-dark">
-        <div className="container"><p className="m-0 text-center text-white">Copyright &copy; {title} 2022</p></div>
+	<footer className="py-5">
+        <div className="container"><p className="m-0 text-center">Copyright &copy; {title} 2022</p></div>
     </footer>
   )
 }
