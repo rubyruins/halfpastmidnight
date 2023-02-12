e40e7d1a389e7afbb798a9ebdb442130ebab94e1
rubyruins <soumya.parekh@somaiya.edu>
2023-02-12 14:05:42 +0530

    fix: Solve some styling issues

--- src/components/bottom/footer.js
+++ src/components/bottom/footer.js
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import { faInstagram, faEmail } from '@fortawesome/free-regular-svg-icons';
+
 
 const Footer = ({ title }) => {
 	
@@ -8,7 +10,19 @@ const Footer = ({ title }) => {
 	
 	return (
 		<footer className="py-5">
-		<div className="container"><p className="m-0 text-center">Copyright &copy; {title} {getCurrentYear()}</p></div>
+			<div className="container social-links">
+				<p className="m-0 mb-2 text-center">
+					<a href="https://www.instagram.com/halfpastmidnight.blog/"><strong className='px-2'>Instagram</strong></a>
+					<a href="mailto:halfpastmidnight.blog@gmail.com
+"><strong className='px-2'>Email</strong></a>
+					<a href="https://www.goodreads.com/user/show/157578393-rubyruins"><strong className='px-2'>Goodreads</strong></a>
+				</p>	
+			</div>
+			<div className="container copyright-text">
+				<p className="m-0 text-center">
+					&copy; {title} {getCurrentYear()}
+				</p>	
+			</div>
 		</footer>
 		)
 	}
