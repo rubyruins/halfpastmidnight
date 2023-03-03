089050fde39d14e1119912c9830463ffed863a17
rubyruins <soumya.parekh@somaiya.edu>
2023-03-03 18:16:47 +0530

    feat: Use changed Instagram handle

--- src/components/bottom/footer.js
+++ src/components/bottom/footer.js
@@ -10,7 +10,7 @@ const Footer = ({ title }) => {
 		<footer className="py-5">
 			<div className="container social-links">
 				<p className="m-0 mb-2 text-center">
-					<a href="https://www.instagram.com/halfpastmidnight.blog/"><strong className='px-2'>Instagram</strong></a>
+					<a href="https://www.instagram.com/halfpastmidnightblog/"><strong className='px-2'>Instagram</strong></a>
 					<a href="mailto:halfpastmidnight.blog@gmail.com
 "><strong className='px-2'>Email</strong></a>
 					<a href="https://www.goodreads.com/user/show/157578393-rubyruins"><strong className='px-2'>Goodreads</strong></a>
